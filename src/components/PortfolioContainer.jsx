ff5c5023080587a6c60536506b5b569d6cd577a0
Kun Wansom <wansom.kun@gmail.com>
2025-01-12 18:12:06 -0800

    setup app

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import NavTabs from "./NavTabs";
+import Resume from "./pages/Resume";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Resume");
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      <main className="mx-3">{renderPage()}</main>
+    </div>
+  );
+}
---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Porfolio() {
+    return (
+      <div>
+        <h1>My works</h1>
+        {/* <p>
+ Add works
+        </p> */}
+      </div>
+    );
+  }
