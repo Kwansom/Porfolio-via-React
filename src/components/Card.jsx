1dbd0d399224cc84f9d90c59b6f3a0354bedfdf3
Kun Wansom <wansom.kun@gmail.com>
2025-01-18 20:16:39 -0800

    got icon to work! need README

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,35 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
 export default function Card({ project }) {
   return (
     <div className="card">
       <img className="card-image" src={project.image}   style={{ width: "100%", height: "auto", borderRadius: "8px" }}  />
       <div className="card-text">
         <h1 className="card-title">{project.title}</h1>
-        <p className="card-github">
-          <a href={project.github}>Link To Github</a>
-        </p>
+        <GitHubIcon
+              id="icon"
+              className="pop-on-hover"
+              sx={{
+                fontSize: 40,
+                cursor: "pointer",
+                "&:hover": {
+                  transform: "scale(1.1)",
+                  color: "gold",
+                },
+              }}
+              onClick={(event) =>
+                (window.location.href = project.github)
+              }
+            />
         <p className="card-deployed">
           <a href={project.deployed}>Link To Deployed</a>
         </p>
+        
       </div>
     </div>
+
+    
   );
 }
