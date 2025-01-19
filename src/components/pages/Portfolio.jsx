aa36653b9fe42b91ce8aa29588581cc9bcc3b30f
Kun Wansom <wansom.kun@gmail.com>
2025-01-18 17:49:31 -0800

    starting portfolio jsx cards

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,18 +1,19 @@
+import React from "react";
 import Card from "../Card";
 
-export default function Porfolio() {
+export default function Portfolio() {
   const projects = [
     {
-      title: "Project 1",
+      title: "MVC Tech Blog",
       image: "https://placehold.co/600x400",
-      github: "http://google.com",
-      deployed: "http://google.com",
+      github: "https://github.com/Kwansom/MVC-Tech-Blog",
+      deployed: "https://mvc-tech-blog-1dzl.onrender.com/",
     },
     {
-      title: "Project 2",
-      image: "https://placehold.co/200x100",
-      github: "http://google.com",
-      deployed: "http://google.com",
+      title: "Music Discovery App",
+      image: "https://placehold.co/600x400",
+      github: "https://github.com/Kwansom/music-discovery-app",
+      deployed: "https://rennancruz.github.io/music-discovery-app/",
     },
     {
       title: "Project 3",
@@ -41,14 +42,14 @@ export default function Porfolio() {
   ];
 
   return (
-    <div>
-      <h1>Work</h1>
+    <section className="portfolio-section">
+      {/* <h1>Work</h1> */}
 
       <div className="card-container">
         {projects.map((project) => {
           return <Card project={project} />;
         })}
       </div>
-    </div>
+    </section>
   );
 }
