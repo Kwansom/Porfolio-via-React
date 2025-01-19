89632c6acbbfa44fee05b3b1da87885017a21689
Kun Wansom <wansom.kun@gmail.com>
2025-01-18 17:17:20 -0800

    styled about, resume, header

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
     <div className="about-container">
       <img src={portrait} alt="me" style={{ width: "200px", height: "auto" }} />
       <p>
-        <h1>About Me:</h1>
+        {/* <h1>About Me:</h1> */}
         Hello, my name is Kun. I am an aspiring web developer. Having worked in
         the fine art industry for close to 10 years- I want to take my analog
         problem solving skills into the digital landscape. You can find my work
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,10 +1,136 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import resume from "../../assets/resume/Resume2023.pdf";
+import "../../assets/css/Resume.css";
+
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      {/* <p>
-Add resume info
-        </p> */}
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: "50px 0",
+        fontFamily: "'Helvetica Neue', sans-serif",
+        color: "#333",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="resume-container">
+        <a
+          href={resume}
+          download="Kun Wansom's Resume"
+          target="_blank"
+          rel="noreferrer"
+          style={{ marginBottom: "50px" }}
+        >
+          <Button
+            id="resume-button"
+            className="pop-on-hover"
+            variant="contained"
+            sx={{
+              fontSize: "18px",
+              fontWeight: "600",
+              textTransform: "none",
+              padding: "12px 40px",
+              borderRadius: "30px",
+              backgroundColor: "lemonchiffon",
+              color: "#222",
+              boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.1)",
+              "&:hover": {
+                backgroundColor: "#f4a300",
+                boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.2)",
+              },
+            }}
+          >
+            Download My Resume Here
+          </Button>
+        </a>
+
+        <h2
+          style={{
+            fontSize: "32px",
+            fontWeight: "700",
+            letterSpacing: "1px",
+            textTransform: "uppercase",
+            color: "#222",
+            marginBottom: "20px",
+            textDecoration: "underline",
+          }}
+        >
+          Front-end Skills
+        </h2>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "20px",
+            lineHeight: "1.8",
+            color: "#555",
+            maxWidth: "800px",
+            margin: "0 auto",
+            borderRadius: "30px",
+            backgroundColor: "lemonchiffon",
+            boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          HTML
+          <br />
+          CSS
+          <br />
+          JavaScript
+          <br />
+          React
+          <br />
+          Bootstrap
+        </p>
+
+        <h2
+          style={{
+            fontSize: "32px",
+            fontWeight: "700",
+            letterSpacing: "1px",
+            textTransform: "uppercase",
+            color: "#222",
+            margin: "15px 0 20px 0",
+            textDecoration: "underline",
+          }}
+        >
+          Back-end Skills
+        </h2>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "20px",
+            lineHeight: "1.8",
+            color: "#555",
+            maxWidth: "800px",
+            margin: "0 auto",
+            borderRadius: "30px",
+            backgroundColor: "lemonchiffon",
+            boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          HTML
+          <br />
+          APIs
+          <br />
+          Node
+          <br />
+          Express
+          <br />
+          PostgreSQL, Sequelize
+          <br />
+          MongoDB, Mongoose
+          <br />
+          Bootstrap
+          <br />
+          REST
+          <br />
+          GraphQL
+        </p>
+      </div>
+    </Box>
   );
 }
