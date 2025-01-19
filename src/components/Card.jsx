f83f9c001ff979bd7688a602138a9e0dafceabfd
Kun Wansom <wansom.kun@gmail.com>
2025-01-18 20:20:19 -0800

    further styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,31 +5,32 @@ import EmailIcon from "@mui/icons-material/Email";
 export default function Card({ project }) {
   return (
     <div className="card">
-      <img className="card-image" src={project.image}   style={{ width: "100%", height: "auto", borderRadius: "8px" }}  />
+      <img
+        className="card-image"
+        src={project.image}
+        style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+      />
       <div className="card-text">
         <h1 className="card-title">{project.title}</h1>
         <GitHubIcon
-              id="icon"
-              className="pop-on-hover"
-              sx={{
-                fontSize: 40,
-                cursor: "pointer",
-                "&:hover": {
-                  transform: "scale(1.1)",
-                  color: "gold",
-                },
-              }}
-              onClick={(event) =>
-                (window.location.href = project.github)
-              }
-            />
+          id="icon"
+          className="pop-on-hover"
+          sx={{
+            fontSize: 40,
+            cursor: "pointer",
+            "&:hover": {
+              transform: "scale(1.1)",
+              color: "gold",
+            },
+          }}
+          onClick={(event) => (window.location.href = project.github)}
+        />
         <p className="card-deployed">
-          <a href={project.deployed}>Link To Deployed</a>
+          <a href={project.deployed} style={{ textDecoration: "none" }}>
+            Link To Deployed
+          </a>
         </p>
-        
       </div>
     </div>
-
-    
   );
 }
